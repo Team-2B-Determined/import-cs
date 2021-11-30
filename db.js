618c124ec5bd3e455fc08e8538d01f349ca45e8b
potato <55855916+potatoinatornado@users.noreply.github.com>
2021-11-30 14:38:09 -0800

    Add central DB

--- db.js
+++ db.js
@@ -9,8 +9,7 @@ require("dotenv").config();
 //   port: process.env.PG_PORT,
 // };
 
-const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
-
+const devConfig = "postgres://mrvuwwyisjimno:e714cf015e8c52869c218cd902d152ae90d7b13f7d13302b852c26c4f87215b3@ec2-34-203-114-67.compute-1.amazonaws.com:5432/d2o4rot4olu8hs";
 const proConfig = process.env.DATABASE_URL; //heroku addons
 
 const pool = new Client({
