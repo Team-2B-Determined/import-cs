1df4cd21418a8d59cadb5b1093628b3926645817
lettertea <34195263+lettertea@users.noreply.github.com>
2022-02-11 13:51:39 -0800

    Populate SelectionSort field when redirected from history

--- client/src/pages/algorithms/SelectionSort.tsx
+++ client/src/pages/algorithms/SelectionSort.tsx
@@ -3,109 +3,113 @@ import {Step} from "../../components/CodeNavigator";
 import displayCodes from "../../codeDisplays";
 import {Button, Col, Container, Form, FormControl, InputGroup} from "react-bootstrap";
 import React, {useState} from "react";
+import {HistoryRow} from "../accounts/History";
+import {useLocation} from "react-router-dom";
 
-const SelectionSort = () => {
-    const [numbersInput, setNumbersInput] = useState<string>("")
-    const [showCalculation, setShowCalculation] = useState<boolean>(false)
+const displayArray = (arr: number[], highlightAtIndexes?: number[]) => {
+    return [...arr].map((num, arrIndex) => {
+        let displayedNum: JSX.Element | string;
+        const isLastElement = arrIndex === arr.length - 1
+        if (highlightAtIndexes?.includes(arrIndex)) {
+            displayedNum = isLastElement ? <strong>{num}</strong> : <><strong>{num}</strong>, </>
+        } else {
+            displayedNum = isLastElement ? num.toString() : num.toString() + ", "
+        }
+        return displayedNum
+    })
+}
 
+const _selectionSort = (arr: number[]): any[] => {
+    const steps: Step[] = []
 
-    const displayArray = (arr: number[], highlightAtIndexes?: number[]) => {
-        return [...arr].map((num, arrIndex) => {
-            let displayedNum: JSX.Element | string;
-            const isLastElement = arrIndex === arr.length - 1
-            if (highlightAtIndexes?.includes(arrIndex)) {
-                displayedNum = isLastElement ? <strong>{num}</strong> : <><strong>{num}</strong>, </>
-            } else {
-                displayedNum = isLastElement ? num.toString() : num.toString() + ", "
-            }
-            return displayedNum
+    for (let i = 0; i < (arr.length - 1); i++) {
+        steps.push({
+            lineNumber: "3",
+            description: <>minInd is initialized to {i} as the element at that index, {arr[i]}, is the
+                smallest value we know as of this iteration <br/>
+                arr=[{displayArray(arr, [i])}]
+            </>
         })
-    }
+        let minInd = i
 
-    const _selectionSort = (arr: number[]): any[] => {
-        const steps: Step[] = []
+        for (let j = (i + 1); j < arr.length; j++) {
 
-        for (let i = 0; i < (arr.length - 1); i++) {
+            if (arr[j] < arr[minInd]) {
+                steps.push({
+                    lineNumber: "5-7",
+                    description: <>The current element, {arr[j]}, is less than {arr[minInd]}, so we set minInd
+                        to {j} now <br/>
+                        arr=[{displayArray(arr, [j])}]
+                    </>
+                })
+                minInd = j
+            }
+        }
+
+        if (minInd !== i) {
             steps.push({
-                lineNumber: "3",
-                description: <>minInd is initialized to {i} as the element at that index, {arr[i]}, is the
-                    smallest value we know as of this iteration <br/>
+                lineNumber: "11-13",
+                description: <>Swap the elements between indexes {i} and {minInd} since a smaller element was
+                    found<br/>
+                    arr=[{displayArray(arr, [i, minInd])}]
+                </>
+            })
+            const temp = arr[minInd];
+            arr[minInd] = arr[i];
+            arr[i] = temp;
+        } else {
+            steps.push({
+                lineNumber: "14-16",
+                description: <>A smaller element than {arr[i]} was not found<br/>
                     arr=[{displayArray(arr, [i])}]
                 </>
             })
-            let minInd = i
-
-            for (let j = (i + 1); j < arr.length; j++) {
-
-                if (arr[j] < arr[minInd]) {
-                    steps.push({
-                        lineNumber: "5-7",
-                        description: <>The current element, {arr[j]}, is less than {arr[minInd]}, so we set minInd
-                            to {j} now <br/>
-                            arr=[{displayArray(arr, [j])}]
-                        </>
-                    })
-                    minInd = j
-                }
-            }
-
-            if (minInd !== i) {
-                steps.push({
-                    lineNumber: "11-13",
-                    description: <>Swap the elements between indexes {i} and {minInd} since a smaller element was
-                        found<br/>
-                        arr=[{displayArray(arr, [i, minInd])}]
-                    </>
-                })
-                const temp = arr[minInd];
-                arr[minInd] = arr[i];
-                arr[i] = temp;
-            } else {
-                steps.push({
-                    lineNumber: "14-16",
-                    description: <>A smaller element than {arr[i]} was not found<br/>
-                        arr=[{displayArray(arr, [i])}]
-                    </>
-                })
-                console.log("No need to swap!")
-            }
+            console.log("No need to swap!")
         }
-        steps.push({
-            lineNumber: "18",
-            description: <>Finished! The array is now sorted!<br/>
-                arr=[{displayArray(arr)}]
-            </>
-        })
-        return steps
     }
+    steps.push({
+        lineNumber: "18",
+        description: <>Finished! The array is now sorted!<br/>
+            arr=[{displayArray(arr)}]
+        </>
+    })
+    return steps
+}
 
 
-    const links: ExternalLink[] = [
-        {
-            name: "GeeksForGeeks",
-            url: "https://www.geeksforgeeks.org/selection-sort/"
-        },
-        {
-            name: "Video (2:42)",
-            url: "https://www.youtube.com/watch?v=g-PGLbMth_g"
-        }
-    ]
+const links: ExternalLink[] = [
+    {
+        name: "GeeksForGeeks",
+        url: "https://www.geeksforgeeks.org/selection-sort/"
+    },
+    {
+        name: "Video (2:42)",
+        url: "https://www.youtube.com/watch?v=g-PGLbMth_g"
+    }
+]
+
+const SelectionSort = ():any => {
+    const location = useLocation();
+    console.log(location.state)
+    const historyRows:HistoryRow[] = JSON.parse(localStorage.getItem("historyRows") || "[]");
 
 
-    // const historyRows = JSON.parse(localStorage.getItem("historyRows") || "[]");
-    // historyRows.push({calculationFeature: "C123123omputations", input: "(111)2 + (10101011)2", link: "/computations"});
-    //
-    // localStorage.setItem("historyRows", JSON.stringify(historyRows));
+    // @ts-ignore
+    const [numbersInput, setNumbersInput] = useState<string>(location.state === null ? "" : location.state.input)
+    const [showCalculation, setShowCalculation] = useState<boolean>(false)
 
     let steps: any[] = _selectionSort(numbersInput.split(/[ ,]+/).map(e => Number(e)))
     const handleSolve = () => {
         steps = _selectionSort(numbersInput.split(/[ ,]+/).map(e => Number(e)))
         setShowCalculation(true)
+        historyRows.push({calculationFeature: "Selection Sort", input: numbersInput,pathname:"/selectionsort", state:`${numbersInput}`
+        });
+
+        localStorage.setItem("historyRows", JSON.stringify(historyRows));
     }
 
     return (
         <>
-
             <CalculatorPage
                 showCalculation={showCalculation}
                 inputs={<Container>
