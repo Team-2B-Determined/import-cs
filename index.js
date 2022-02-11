3d75a67f30838c299755c3656a196b9848cfc42d
Pikakashi <47730246+Pikakashi@users.noreply.github.com>
2022-02-11 11:15:50 -0800

    Revert "Merge branch 'master' of https://github.com/Team-2B-Determined/import-cs"
    This reverts commit e14ff2ebc06e011f6dbc9386903c52e84c1c37d2, reversing
    changes made to 9294e2546f76d999e75184216b72a6c0da5908b6.

--- index.js
+++ index.js
@@ -18,11 +18,11 @@ app.use(express.json()); // => allows us to access the req.body
 if (process.env.NODE_ENV === "production") {
   //server static content
   //npm run build
-  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.use(express.static(path.join(__dirname, "client/build")));
 }
 
 console.log(__dirname);
-console.log(path.join(__dirname, "../client/build"));
+console.log(path.join(__dirname, "client/build"));
 
 //ROUTES//
 
@@ -101,7 +101,7 @@ app.delete("/todos/:id", async (req, res) => {
 });
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  res.sendFile(path.join(__dirname, "client/build/index.html"));
 });
 
 app.listen(PORT, () => {
