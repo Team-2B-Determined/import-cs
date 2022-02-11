c10e0f67a250007274b72d3f27431262a831da1b
lettertea <34195263+lettertea@users.noreply.github.com>
2022-02-11 12:12:56 -0800

    Remove trailing comma in array presentation

--- client/src/pages/algorithms/SelectionSort.tsx
+++ client/src/pages/algorithms/SelectionSort.tsx
@@ -9,9 +9,17 @@ const SelectionSort = () => {
     const [showCalculation, setShowCalculation] = useState<boolean>(false)
 
 
-    const displayArray = (arr: any, highlightAtIndexes?: number[]) => {
-        return [...arr].map((num, arrIndex) => highlightAtIndexes?.includes(arrIndex) ? <>
-            <strong>{num}</strong>, </> : num + ", ")
+    const displayArray = (arr: number[], highlightAtIndexes?: number[]) => {
+        return [...arr].map((num, arrIndex) => {
+            let displayedNum: JSX.Element | string;
+            const isLastElement = arrIndex === arr.length - 1
+            if (highlightAtIndexes?.includes(arrIndex)) {
+                displayedNum = isLastElement ? <strong>{num}</strong> : <><strong>{num}</strong>, </>
+            } else {
+                displayedNum = isLastElement ? num.toString() : num.toString() + ", "
+            }
+            return displayedNum
+        })
     }
 
     const _selectionSort = (arr: number[]): any[] => {
